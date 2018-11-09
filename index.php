9e7982312457655b25a8e57622cb5e20f421f2e9
Unknown <delasalle.sio.colleu.e@gmail.com>
2018-11-09 09:58:15 +0100

    Création début site

---
+++ index.php
@@ -0,0 +1,52 @@
+<DOCTYPE HTML>
+	<!--<head>
+    <title>Une boutique simple Amazon</title>
+    <meta name='author' lang='fr' content='Yann Geffrotin'/>
+    <meta name='email' content='yann@geffrotin.fr'/>
+    <meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'/>
+
+    <link rel='stylesheet' media='screen' type='text/css' title='index' href='style.css' />
+	</head>	
+	<body>
+		<nav role="navigation" id="primary-nav">
+		<ul>
+		<li><div class="Haut" >Un Titre mdr</div></li>
+		<li><a href="#">lien b</a></li>
+		</ul>
+		</nav>
+
+		<div class="BarreGauche">test barre gauche </div>
+	</body>-->
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Râ'Smoothie</title>
+        <link rel="stylesheet" type="text/css" href="Styles/style.css" />
+        <link rel="icon" type="image/png" href="Images/logo.png" />
+<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /><![endif]-->
+    </head>
+       
+    <body>
+        <header>
+            <img src="Images/bandeau.jpg" width=50%  margin-left=0 alt="ombre" title="ombrage" class="redim_img"/>         
+        </header>
+       
+        <nav>
+			<button>un bouton</button>
+        </nav>
+       
+        <section>
+			<table>
+            <img src="logo.gif" width=50%  margin-left=0 alt="ombre" title="ombrage" class="redim_img"/>   
+        </section>
+       
+        <footer>
+			<p>une.adresse@oui.bzh</p>
+        </footer>
+    </body>
+   
+   
+</html>
+
+
+
