2a32b22001b8ff23ecdc8beca425d084d5cc8067
Elven <delasalle.sio.colleu.e@gmail.com>
2018-11-17 13:19:37 +0100

    debut mise en forme

--- accueil.php
+++ accueil.php
@@ -19,6 +19,35 @@
 <html>
 	<!-- Début du corps -->  
 	<body>
-		
+		<!-- Création pour un smoothie -->
+		<table class="table">
+			<thead class="thead-dark">
+				<tr>					
+					<h2>
+						<center>Nos Smoothies</center>
+					</h2>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+				<tr>
+					<th scope="row">2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
+				</tr>
+				<tr>
+					<th scope="row">3</th>
+					<td>Larry</td>
+					<td>the Bird</td>
+					<td>@twitter</td>
+				</tr>
+			</tbody>
+		</table>		
 	</body>
 </html>
