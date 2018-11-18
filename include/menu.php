8a1136ef10ef26fa2478f8f1d50c8fe0a939d8d5
couillard <couillard.sio.t@gmail.com>
2018-11-18 21:10:45 +0100

    image + correction menu

--- include/menu.php
+++ include/menu.php
@@ -9,7 +9,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<a class="navbar-brand" href="accueil.php">
-		<img src="images/logo" width="30" height="30" alt="">
+		<img src="images/logo.png" width="30" height="30" alt="">
 		Râ'Smoothie
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
