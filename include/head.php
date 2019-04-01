af16c7b872ef5894ba4745f90b2163bc67b2236d
Unknown <delasalle.sio.colleu.e@gmail.com>
2019-04-01 10:28:16 +0200

    include d'ajax avec bootstrap

--- include/head.php
+++ include/head.php
@@ -9,9 +9,12 @@
 	<meta name="Content-Language" content="fr">  
 	<meta name="Author" content="Colleu Elven">
 	
-	<!-- access to style sheets -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	
 	<link href="styles/style.css" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
 </head>
